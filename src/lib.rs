7064db3cdd924efc7fa7c00664920a2b482e7bcf
Vasily Styagov <styagov.dev@gmail.com>
2023-08-16 17:22:39 +0100

    Make generated method payable

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 use quote::quote;
-use syn::{DeriveInput, parse_macro_input};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(SelfUpdate)]
 pub fn my_derive(input: TokenStream) -> TokenStream {
@@ -11,8 +11,10 @@ pub fn my_derive(input: TokenStream) -> TokenStream {
     let name = input.ident;
 
     let expanded = quote! {
+        #[near_bindgen]
         impl #name {
-            pub fn update_contract(&self) -> Promise {
+            #[payable]
+            pub fn update_contract(&mut self) -> Promise {
                 assert!(env::predecessor_account_id() == self.manager, "Only the manager can update the code");
                 assert_one_yocto();
 
@@ -20,12 +22,7 @@ pub fn my_derive(input: TokenStream) -> TokenStream {
 
                 Promise::new(env::current_account_id())
                     .deploy_contract(code)
-                    .function_call(
-                        "migrate".to_string(),
-                        vec![],
-                        0,
-                        Gas(200_000_000_000_000),
-                    )
+                    .function_call("migrate".to_string(), vec![], 0, Gas(200_000_000_000_000))
                     .as_return()
             }
         }
--- tests/test.rs
+++ tests/test.rs
@@ -1,17 +1,20 @@
-use near_sdk::AccountId;
-use near_sdk::{assert_one_yocto, env, Gas, Promise};
-use near_self_update::SelfUpdate;
+#[cfg(test)]
+mod tests {
+    use near_sdk::{assert_one_yocto, env, near_bindgen, AccountId, Gas, Promise};
+    use near_self_update::SelfUpdate;
 
-#[derive(SelfUpdate)]
-struct Contract {
-    pub manager: AccountId,
-}
+    #[derive(SelfUpdate)]
+    #[near_bindgen]
+    pub struct Contract {
+        pub manager: AccountId,
+    }
 
-#[test]
-#[should_panic(expected = "Only the manager can update the code")]
-fn test_macro() {
-    let contract = Contract {
-        manager: AccountId::new_unchecked("alice".to_string()),
-    };
-    contract.update_contract();
+    #[test]
+    #[should_panic(expected = "Only the manager can update the code")]
+    fn test_macro() {
+        let mut contract = Contract {
+            manager: AccountId::new_unchecked("alice".to_string()),
+        };
+        contract.update_contract();
+    }
 }
